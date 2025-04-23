847a8ee5c8521b0062bb551b0bfae944c5127434
Rishijangir566 <rishijangirjr@gmail.com>
2025-04-23 10:40:50 +0530

    style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,87 @@
-import { useState } from 'react'
-import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
+import { RxCrossCircled } from "react-icons/rx";
 function App() {
-
-  const [note, setNote] = useState([])
+  const [note, setNote] = useState([]);
   const [form, setForm] = useState({
     text: "",
-    color: ""
-  })
+    color: "",
+  });
 
   function handleForm(e) {
     const { name, value } = e.target;
-    setForm({ ...form, [name]: value })
+    setForm({ ...form, [name]: value });
   }
 
   function addTask(e) {
-    e.preventDefault()
-    const obj = {id:Date.now(), text: form.text, color: form.color }
-    setNote([...note, obj])
-  }
-   function deleteItem(idToDelete){
-    setNote(note.filter((box)=>box.id !== idToDelete))
-   }
+    e.preventDefault();
+    const obj = { id: Date.now(), text: form.text, color: form.color };
+    setNote([...note, obj]);
+    setForm({text:""})
 
-  console.log(note);
+  }
+  function deleteItem(idToDelete) {
+    setNote(note.filter((box) => box.id !== idToDelete));
+  }
+
+  // console.log(note);
 
   return (
     <>
-        <h2> * Notepad *</h2>
-      <form className="left" onSubmit={addTask}>
-        <textarea placeholder='Add your Note' name='text' rows={8} cols={40} value={form.text} onChange={handleForm} required> </textarea> <br />
-        <input type="color" value={form.color} name='color' onChange={handleForm} required/>
-        <button type='submit' >AddTask</button>
-      </form>
+      <div className="container ">
+        <h2> * Notepad * </h2>
+        <div className="boxes">
 
-      <div className="right">
-        <h1>Your Notes</h1>
-        <div className='outer'>
-          {note.length > 0 ? (
-            note.map((item) => {
-              return <div className="box" style={{ backgroundColor: item.color }} key={item.id}>  {item.text} <span><RxCross2 onClick={()=>deleteItem(item.id)} /> </span> </div>
-            })
-          )
-            : "you have not added a note yet"}
+        <div className="left">
+          <form onSubmit={addTask}>
+            <textarea
+              placeholder="Add your Note"
+              name="text"
+              rows={8}
+              cols={40}
+              value={form.text}
+              onChange={handleForm}
+              required
+              >
+              
+            </textarea>
+            <br />
+            <input
+              type="color"
+              value={form.color}
+              name="color"
+              onChange={handleForm}
+              required
+              />
+            <button type="submit">AddTask</button>
+          </form>
         </div>
+
+        <div className="right">
+          <h1>Your Notes</h1>
+          <div className="outer">
+            {note.length > 0
+              ? note.map((item) => {
+                return (
+                  <div
+                  className="box"
+                  style={{ backgroundColor: item.color }}
+                  key={item.id}
+                  >
+                    
+                      {item.text}
+                      <span>
+                        <RxCrossCircled onClick={() => deleteItem(item.id)} />{" "}
+                      </span>{" "}
+                    </div>
+                  );
+                })
+                : "you have not added a note yet"}
+          </div>
+        </div>
+                </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
